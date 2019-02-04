106b82f50ca6715e46ac55f2be4ee48d3f7b6877
Deadpikle <mbabienco@southern.edu>
2019-02-04 14:10:19 +0700

    Basic file/folder adding works

---
+++ EasyBackup/Models/FolderFileItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyBackup.Models
+{
+    class FolderFileItem
+    {
+        public string Path { get; set; }
+        public bool IsDirectory { get; set; }
+        public bool IsRecursive { get; set; }
+        public bool OnlyCopiesLatestFile { get; set; }
+    }
+}
--- EasyBackup/ViewModels/HomeScreenViewModel.cs
+++ EasyBackup/ViewModels/HomeScreenViewModel.cs
@@ -1,29 +1,80 @@
 ﻿using EasyBackup.Helpers;
 using EasyBackup.Interfaces;
+using EasyBackup.Models;
+using GongSolutions.Wpf.DragDrop;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace EasyBackup.ViewModels
 {
-    class HomeScreenViewModel : BaseViewModel
+    class HomeScreenViewModel : BaseViewModel, IDropTarget
     {
+        private ObservableCollection<FolderFileItem> _items;
 
         public HomeScreenViewModel(IChangeViewModel viewModelChanger) : base(viewModelChanger)
         {
+            Items = new ObservableCollection<FolderFileItem>();
         }
 
-        public ICommand MoveToAnotherScreen
+        public ObservableCollection<FolderFileItem> Items
         {
-            get { return new RelayCommand(LoadAnotherScreen); }
+            get { return _items; }
+            set { _items = value; NotifyPropertyChanged(); }
         }
 
-        private void LoadAnotherScreen()
+        public ICommand AddFolder
         {
-            PushViewModel(new AnotherScreen(ViewModelChanger));
+            get { return new RelayCommand(ChooseFolder); }
         }
+
+        private void ChooseFolder()
+        {
+            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            dialog.ShowNewFolderButton = true;
+            if (dialog.ShowDialog(Application.Current.MainWindow).GetValueOrDefault())
+            {
+                Items.Add(new FolderFileItem() { Path = dialog.SelectedPath, IsRecursive = true });
+            }
+        }
+
+        public ICommand AddFile
+        {
+            get { return new RelayCommand(ChooseFile); }
+        }
+
+        private void ChooseFile()
+        {
+            var dialog = new Ookii.Dialogs.Wpf.VistaOpenFileDialog();
+            dialog.Multiselect = true;
+            dialog.ShowReadOnly = true;
+            dialog.Title = "Choose a file";
+            if (dialog.ShowDialog(Application.Current.MainWindow).GetValueOrDefault())
+            {
+                foreach (string fileName in dialog.FileNames)
+                {
+                    Items.Add(new FolderFileItem() { Path = fileName });
+                }
+            }
+        }
+
+        #region IDropTarget
+
+        public void DragOver(IDropInfo dropInfo)
+        {
+            dropInfo.DropTargetAdorner = DropTargetAdorners.Highlight;
+            dropInfo.Effects = DragDropEffects.Copy;
+        }
+
+        public void Drop(IDropInfo dropInfo)
+        {
+        }
+
+        #endregion
     }
 }
